d610eeeb11bce01366d5c84b5e96f5910ecef3fd
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-09-16 03:27:09 -0700

    switched to prod firebase app

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -7,14 +7,24 @@ import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAaiwgYeV1mlYWjfkLscUsQ_K8nAiC9ims",
+//   authDomain: "beter-than-you-society.firebaseapp.com",
+//   projectId: "better-than-you-society",
+//   storageBucket: "better-than-you-society.appspot.com",
+//   messagingSenderId: "821349551727",
+//   appId: "1:821349551727:web:0801041ff1ab6bd325c79a",
+//   measurementId: "G-R4EW174VXJ"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAaiwgYeV1mlYWjfkLscUsQ_K8nAiC9ims",
-  authDomain: "beter-than-you-society.firebaseapp.com",
-  projectId: "better-than-you-society",
-  storageBucket: "better-than-you-society.appspot.com",
-  messagingSenderId: "821349551727",
-  appId: "1:821349551727:web:0801041ff1ab6bd325c79a",
-  measurementId: "G-R4EW174VXJ"
+	apiKey: "AIzaSyCuMkR9ep3Aj_L3RZ5Zvir0iULbz8lIM_A",
+	authDomain: "btys-production.firebaseapp.com",
+	projectId: "btys-production",
+	storageBucket: "btys-production.appspot.com",
+	messagingSenderId: "269241867312",
+	appId: "1:269241867312:web:eecccc5fa387aa444de1f5",
+	measurementId: "G-ED6C9BHHNB"
 };
 
 // Initialize Firebase
