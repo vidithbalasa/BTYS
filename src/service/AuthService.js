ca0096f5cf932da907a9bbcb3f9b40d5b46cd713
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-09-15 21:48:39 -0700

    added google logo and updated login page, functional build

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -3,33 +3,26 @@ import { getApp } from 'firebase/app';
 import { auth } from '../config/firebase.config';
 
 class AuthService {
-  constructor() {
-    this.auth = getAuth();
-  }
-
   login() {
-    return signInWithPopup(
-        this.auth, 
-        new GoogleAuthProvider()
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-                console.log(`User: ${user}`);
-            })
-            .catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                console.log(`Error: ${errorMessage}`);
-            })
-    );
+    return signInWithPopup(auth, new GoogleAuthProvider())
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(`User: ${user}`);
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(`Error: ${errorMessage}`);
+        })
   }
 
   async logout() {
