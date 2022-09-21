6983d125d79743560c82364bef3638cdec4df77d
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-09-20 18:30:21 -0700

    Testing calling cloud function

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,29 +2,22 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAaiwgYeV1mlYWjfkLscUsQ_K8nAiC9ims",
-//   authDomain: "beter-than-you-society.firebaseapp.com",
-//   projectId: "better-than-you-society",
-//   storageBucket: "better-than-you-society.appspot.com",
-//   messagingSenderId: "821349551727",
-//   appId: "1:821349551727:web:0801041ff1ab6bd325c79a",
-//   measurementId: "G-R4EW174VXJ"
-// };
+const is_dev = process.env.NODE_ENV === "development";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCuMkR9ep3Aj_L3RZ5Zvir0iULbz8lIM_A",
-	authDomain: "btys-production.firebaseapp.com",
-	projectId: "btys-production",
-	storageBucket: "btys-production.appspot.com",
-	messagingSenderId: "269241867312",
-	appId: "1:269241867312:web:eecccc5fa387aa444de1f5",
-	measurementId: "G-ED6C9BHHNB"
+  apiKey: is_dev ? "AIzaSyAaiwgYeV1mlYWjfkLscUsQ_K8nAiC9ims" : "AIzaSyCuMkR9ep3Aj_L3RZ5Zvir0iULbz8lIM_A",
+  authDomain: is_dev ? "beter-than-you-society.firebaseapp.com" : "btys-production.firebaseapp.com",
+  projectId: is_dev ? "better-than-you-society" : "btys-production",
+  storageBucket: is_dev ? "better-than-you-society.appspot.com" : "btys-production.appspot.com",
+  messagingSenderId: is_dev ? "821349551727" : "269241867312",
+  appId: is_dev ? "1:821349551727:web:0801041ff1ab6bd325c79a" : "1:269241867312:web:eecccc5fa387aa444de1f5",
+  measurementId: is_dev ? "G-R4EW174VXJ" : "G-ED6C9BHHNB"
 };
 
 // Initialize Firebase
@@ -38,6 +31,8 @@ if (!getApps.length) {
 }
 
 export const auth = getAuth(getApp());
-if (process.env.NODE_ENV == 'development') {
+if (is_dev) {
   connectAuthEmulator(auth, "http://localhost:9099");
 }
+
+export const functions = getFunctions(getApp());
