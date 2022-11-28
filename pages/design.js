fe0423e5994917704a8f390efe6886e9c3c740ef
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-11-28 15:36:14 -0800

    Added prompt input

--- pages/design.js
+++ pages/design.js
@@ -9,36 +9,30 @@ function Design() {
     const functions = getFunctions(getApp());
 
     const callFunction = async () => {
-        // const corsHandler = cors({ origin: true });
         // create a reponse to the stableai-function with the header Access-Control-Allow-Origin: *
         const stableaiCall = httpsCallable(functions, 'stableai-function')
-        await stableaiCall({ data: 'test' })
+        await stableaiCall({ prompt: prompt })
             .then((result) => {
                 console.log(result);
             })
             .catch((error) => {
                 console.log(error);
             });
-
-        // const response = await stableaiCall()
-        //     .then((result) => {
-        //         // Read result of the Cloud Function.
-        //         const data = result.data;
-        //         const sanitizedMessage = data.text;
-        //         console.log(sanitizedMessage);
-        //     })
-        //     .catch((error) => {
-        //         // Getting the Error details.
-        //         const code = error.code;
-        //         const message = error.message;
-        //         const details = error.details;
-        //         console.log(code, message, details);
-        //     });
     }
 
     return (
         <main>
-            <h1>Create an Image</h1>
+            <h1>Generate an Image</h1>
+            <div className='prompt-input'>
+                <label htmlFor='prompt'>Input a prompt</label>
+                <input
+                    type='text'
+                    name='prompt'
+                    id='prompt'
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                />
+            </div>
             <div>
                 <button onClick={callFunction}>Call Function</button>
             </div>
