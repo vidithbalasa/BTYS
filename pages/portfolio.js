68effcc294066ded395dafccd8ada6efab8b1cb8
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-11-29 21:40:01 -0800

    fixed bug

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,7 +3,7 @@ import useAuth from '../src/auth/authContext';
 import { getFirestore, collection, getDocs, doc } from 'firebase/firestore';
 import '../styles/portfolio.module.css';
 import { withProtected } from '../src/auth/route';
-import { Image } from 'next/image';
+import Image from 'next/image';
 
 const Portfolio = () => {
     const [images, setImages] = useState([]);
