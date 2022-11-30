07d5f3b2b00756084e0342ef5f65ba8ded29abf4
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-11-29 20:31:39 -0800

    Added SSG to portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,38 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../src/auth/authContext';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import '../styles/portfolio.css';
 
-export default function Portfolio() {
-    const [images, setImages] = useState([]);
+export default function Portfolio({ images }) {
+    // const [images, setImages] = useState([]);
     const auth = useAuth();
 
-    useEffect(() => {
-        getCatalog();
-    }, []);
+    // useEffect(() => {
+    //     getCatalog();
+    // }, []);
 
-    const getCatalog = async () => {
-        const db = getFirestore();
-        // Collection 'users' -> Document 'auth.user.uid' -> Collection 'images'
-        const imagesRef = collection(db, 'users', auth.user.uid, 'images');
-        const catalog = await getDocs(imagesRef);
-        // Get the "url" field from each document and add it to the images array
-        catalog.forEach((doc) => {
-            setImages((imgs) => [...imgs, doc.data().url]);
-        });
-    }
+    // const getCatalog = async () => {
+    //     const db = getFirestore();
+    //     // Collection 'users' -> Document 'auth.user.uid' -> Collection 'images'
+    //     const imagesRef = collection(db, 'users', auth.user.uid, 'images');
+    //     const catalog = await getDocs(imagesRef);
+    //     // Get the "url" field from each document and add it to the images array
+    //     catalog.forEach((doc) => {
+    //         setImages((imgs) => [...imgs, doc.data().url]);
+    //     });
+    // }
 
     return (
         <main>
-            <h1>Portfolio</h1>
-            {
-                images.map((imageUrl, index) => {
-                    return (
-                        <div key={index}>
-                            <img src={imageUrl} alt='Generated Image' />
-                        </div>
-                    );
-                })
-            }
+            <h1 className='title'>Portfolio</h1>
+            <div className='portfolio'>
+                {
+                    images.map((doc, index) => {
+                        return (
+                            <div key={index}>
+                                <img src={doc.data().url} alt='Generated Image' />
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </main>
     );
+}
+
+export async function getStaticProps() {
+    const db = getFirestore();
+    // Collection 'users' -> Document 'auth.user.uid' -> Collection 'images'
+    const imagesRef = collection(db, 'users', auth.user.uid, 'images');
+    const catalog = await getDocs(imagesRef);
+    // Return the images
+    return {
+        props: {
+            images: catalog.docs,
+        }
+    };
 }
