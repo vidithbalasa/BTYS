af917c4a6d13259384ed5603795eb938c53c24c3
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-12-06 15:56:53 -0800

    Added printify function for product info

--- pages/catalog/[blueprint].js
+++ pages/catalog/[blueprint].js
@@ -1,14 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from "next/router";
+import { getFunctions, httpsCallable } from 'firebase/functions';
+import useAuth from '../../src/auth/authContext';
 
 export default function CatalogItem() {
+    const [unique, setUnique] = useState([]);
+    const [variants, setVariants] = useState([]);
     const [item, setItem] = useState({});
     const router = useRouter();
     const { blueprint } = router.query;
+    const functions = getFunctions();
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const getInfo = async () => {
+            const getBlueprintInfo = httpsCallable(functions, 'getBlueprintInfo');
+            await getBlueprintInfo({ blueprint: blueprint, token: user.accessToken })
+                .then((result) => {
+                    const { unique_values, variants } = result.data;
+                    setUnique(unique_values);
+                    setVariants(variants);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        getInfo();
+    }, [blueprint]);
 
     return (
         <div>
             <h1>{blueprint}</h1>
+            {/* button to log unique values */}
+            <button onClick={() => console.log(unique)}>Log Unique Values</button>
+            {/* button to log variants */}
+            <button onClick={() => console.log(variants)}>Log Variants</button>
         </div>
     )
 }
