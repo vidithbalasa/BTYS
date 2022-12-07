11b2b1a913f88d9746ee1a341ad1b819fe04d146
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-12-07 01:41:08 -0800

    styled item page

--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -1,20 +1,11 @@
 import { useEffect, useState, useRef } from 'react';
-import { useRouter } from 'next/router';
 import globalStyles from '../../styles/global.module.css';
 import styles from '../../styles/catalog.module.css';
 import { searchClient } from '../../src/config/firebase.config';
 import { findResultsState } from 'react-instantsearch-dom/server';
 import Search from '../../components/search';
 
-const updateAfter = 700;
-
 const createURL = (state) => `?${qs.stringify(state)}`;
-
-const pathToSearchState = (path) =>
-  path.includes('?') ? qs.parse(path.substring(path.indexOf('?') + 1)) : {};
-
-const searchStateToURL = (searchState) =>
-  searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : '';
 
 export default function Catalog(props) {
     return (
@@ -31,16 +22,13 @@ export default function Catalog(props) {
 }
 
 export async function getServerSideProps({ resolvedUrl }) {
-    // const searchState = pathToSearchState(resolvedUrl);
     const resultsState = await findResultsState(Search, {
         searchClient,
         indexName: 'catalog',
-        // searchState,
     });
 
     return {
         props: {
-            // searchState,
             resultsState: JSON.parse(JSON.stringify(resultsState))
         }
     }
