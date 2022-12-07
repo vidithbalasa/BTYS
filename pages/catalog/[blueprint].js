f058f61cb70cd1a3b432a23f5b5468bf14f481f6
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-12-06 18:49:47 -0800

    Working through item page

--- pages/catalog/[blueprint].js
+++ pages/catalog/[blueprint].js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from "next/router";
 import { getFunctions, httpsCallable } from 'firebase/functions';
+import { getFirestore, collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import useAuth from '../../src/auth/authContext';
 
-export default function CatalogItem() {
+export default function CatalogItem({ item }) {
     const [unique, setUnique] = useState([]);
     const [variants, setVariants] = useState([]);
-    const [item, setItem] = useState({});
+    // const [item, setItem] = useState({});
+
     const router = useRouter();
     const { blueprint } = router.query;
+    
     const functions = getFunctions();
     const { user } = useAuth();
 
@@ -37,6 +40,46 @@ export default function CatalogItem() {
             <button onClick={() => console.log(unique)}>Log Unique Values</button>
             {/* button to log variants */}
             <button onClick={() => console.log(variants)}>Log Variants</button>
+            {/* button to log item */}
+            <button onClick={() => console.log(item)}>Log Item</button>
         </div>
     )
+}
+
+export async function getStaticPaths() {
+    // Get a reference to the firestore instance
+    const firestore = getFirestore();
+  
+    // Query the 'printify_products' collection to get a list of all product names
+    const productCollection = collection(firestore, 'printify_products')
+    const productNames = await getDocs(productCollection)
+
+    // Map the product names to a list of paths
+    const paths = productNames.docs.map(doc => ({
+        params: { id: doc.id }
+    }))
+  
+    // Return a list of paths with the dynamic IDs set to the product names
+    return {
+      paths: paths,
+      fallback: false
+    }
+}
+
+// Get the info for the product from firestore
+export async function getStaticProps({ params }) {
+    // Get a reference to the firestore instance
+    const firestore = getFirestore();
+  
+    // Get the document from the 'printify_products' collection with the name matching the dynamic ID
+    const item_doc = doc(firestore, 'printify_products', params.blueprint);
+    const item = await getDoc(item_doc);
+
+  
+    // Return the product data as props
+    return {
+      props: {
+        item: item.data()
+      }
+    }
 }
