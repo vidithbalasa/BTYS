1d928a9f7cb0e04e14387069f0d2a6326b2e88d1
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-12-06 22:01:55 -0800

    Added image carousel with framer motion

--- pages/catalog/[blueprint].js
+++ pages/catalog/[blueprint].js
@@ -6,12 +6,16 @@ import useAuth from '../../src/auth/authContext';
 import styles from '../../styles/item.module.css'
 import globalStyles from '../../styles/global.module.css'
 import Image from 'next/image';
+import sample_images from '../../public/images.js'
+import { motion, AnimatePresence, useCycle } from 'framer-motion';
 
 export default function CatalogItem({ item }) {
     const [unique, setUnique] = useState([]);
     const [variants, setVariants] = useState([]);
-    // const [currImage, setCurrImage] = useState(0);
-    // const [item, setItem] = useState({});
+    // list of numbers 0 to sample_images.length - 1
+    const NUM_IMAGES = sample_images.length;
+    const indices = Array.from({ length: NUM_IMAGES-1 }, (value, index) => index + 1);
+    const [currentIndex, setCurrentIndex] = useCycle(...indices);
 
     const router = useRouter();
     const { blueprint } = router.query;
@@ -38,61 +42,68 @@ export default function CatalogItem({ item }) {
     }, [blueprint]);
 
     return (
-        <main className={globalStyles.main}>
-            <h1>{item.name}</h1>
-            <div className={styles.display}>
-                <div className={styles.images}>
-                    {/* <button className={styles.scrollLeft} onClick={e => setCurrImage(img => img - 1)}>&lt;</button> */}
-                    <div className={styles.slider_container}>
-                        {
-                            item.image_urls.map((url, i) => {
-                                return (
-                                    <Image src={url} alt={item.name} width={250} height={250} key={i} className={styles.slider_image} />
-                                )
-                            })
-                        }
-                    </div>
-                    {/* <button className={styles.scrollRight} onClick={e => setCurrImage(img => img + 1)}>&gt;</button> */}
-                </div>
-            </div>
+        <main>
+            <button onClick={() => setCurrentIndex(idx => idx + 1)}>Next</button>
+            <button onClick={() => setCurrentIndex(idx => idx - 1)}>Prev</button>
+
+            <motion.div initial={{ x: '-100%' }} animate={{ x: 0 }}>
+                <AnimatePresence>
+                    {
+                        sample_images.map((image, index) => {
+                            return (
+                                <motion.img
+                                    key={index}
+                                    src={image}
+                                    alt="product-image"
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.5 }}
+                                    style={{ display: index === currentIndex ? 'block' : 'none' }}
+                                />
+                            )
+                        })
+                    }
+                </AnimatePresence>
+            </motion.div>
         </main>
     )
 }
 
-export async function getStaticPaths() {
-    // Get a reference to the firestore instance
-    const firestore = getFirestore();
+// export async function getStaticPaths() {
+//     // Get a reference to the firestore instance
+//     const firestore = getFirestore();
   
-    // Query the 'printify_products' collection to get a list of all product names
-    const productCollection = collection(firestore, 'printify_products')
-    const productNames = await getDocs(productCollection)
+//     // Query the 'printify_products' collection to get a list of all product names
+//     const productCollection = collection(firestore, 'printify_products')
+//     const productNames = await getDocs(productCollection)
 
-    // Map the product names to a list of paths
-    const paths = productNames.docs.map(doc => ({
-        params: { blueprint: doc.id }
-    }))
+//     // Map the product names to a list of paths
+//     const paths = productNames.docs.map(doc => ({
+//         params: { blueprint: doc.id }
+//     }))
   
-    // Return a list of paths with the dynamic IDs set to the product names
-    return {
-      paths: paths,
-      fallback: false
-    }
-}
+//     // Return a list of paths with the dynamic IDs set to the product names
+//     return {
+//       paths: paths,
+//       fallback: false
+//     }
+// }
 
-// Get the info for the product from firestore
-export async function getStaticProps({ params }) {
-    // Get a reference to the firestore instance
-    const firestore = getFirestore();
+// // Get the info for the product from firestore
+// export async function getStaticProps({ params }) {
+//     // Get a reference to the firestore instance
+//     const firestore = getFirestore();
   
-    // Get the document from the 'printify_products' collection with the name matching the dynamic ID
-    const item_doc = doc(firestore, 'printify_products', params.blueprint);
-    const item = await getDoc(item_doc);
+//     // Get the document from the 'printify_products' collection with the name matching the dynamic ID
+//     const item_doc = doc(firestore, 'printify_products', params.blueprint);
+//     const item = await getDoc(item_doc);
 
   
-    // Return the product data as props
-    return {
-      props: {
-        item: item.data()
-      }
-    }
-}
+//     // Return the product data as props
+//     return {
+//       props: {
+//         item: item.data()
+//       }
+//     }
+// }
