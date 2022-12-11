9020deed5b1632b0a037732d687d280b3600f07b
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-12-10 16:23:27 -0800

    tweaks

--- pages/catalog/[blueprint].js
+++ pages/catalog/[blueprint].js
@@ -159,20 +159,18 @@ export async function getStaticProps({ params }) {
     const printers = await getDocs(printers_docs);
     // For each doc, get all the varaints
     let variants = [];
-    printers.forEach(printer => {
-        let printer_variants = printer.data().variants;
-        // Variants = {[variant_id]: {'color': 'red', 'size': 'small', 'price': 10}, ...}
-        // Turn it into : [{variant_id: '...', 'size': 'small', 'color': 'red', 'price': 10, 'printer_id': '5'}, ...]
-        formatted_variants = Object.keys(printer_variants).map(variant_id => {
-            return {
-                ...printer_variants[variant_id],
-                variant_id: variant_id,
-                printer_id: printer.id
-            }
-        });
-        variants = variants.concat(formatted_variants);
-    });
-
+    // Variants = {[variant_id]: {'color': 'red', 'size': 'small', 'price': 10}, ...}
+    // Turn it into : [{variant_id: '...', 'size': 'small', 'color': 'red', 'price': 10, 'printer_id': '5'}, ...]
+    for (const printer of printers.docs) {
+        const printer_variants = printer.data().variants;
+        // printer_variants is an object mapping variant_id to variant data
+        for (const variant_id in Object.keys(printer_variants)) {
+            const variant = printer_variants[variant_id];
+            variant['variant_id'] = variant_id;
+            variant['printer_id'] = printer.id;
+            variants.push(variant);
+        }
+    }
   
     // Return the product data as props
     return {
