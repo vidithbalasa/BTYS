0cfd52b1597a5fd2c4e6c6798cd47589ee0c6495
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-12-10 16:33:59 -0800

    fixed firestore call

--- pages/catalog/[blueprint].js
+++ pages/catalog/[blueprint].js
@@ -158,27 +158,21 @@ export async function getStaticProps({ params }) {
     const printers_docs = collection(item_doc, 'print_providers');
     const printers = await getDocs(printers_docs);
     // Turn printers into a format that can be used by the CatalogItem component
+    const variants = printers.docs.map(printer => {
+        const printer_variants = printer.data().variants;
+        for (const variant_id of Object.keys(printer_variants)) {
+            const variant = printer_variants[variant_id];
+            variant['variant_id'] = variant_id;
+            variant['printer_id'] = printer.id;
+            return variant;
+        }
+    })
 
-    // // For each doc, get all the varaints
-    // let variants = [];
-    // // Variants = {[variant_id]: {'color': 'red', 'size': 'small', 'price': 10}, ...}
-    // // Turn it into : [{variant_id: '...', 'size': 'small', 'color': 'red', 'price': 10, 'printer_id': '5'}, ...]
-    // for (const printer of printers.docs) {
-    //     const printer_variants = printer.data().variants;
-    //     // printer_variants is an object mapping variant_id to variant data
-    //     for (const variant_id in Object.keys(printer_variants)) {
-    //         const variant = printer_variants[variant_id];
-    //         variant['variant_id'] = variant_id;
-    //         variant['printer_id'] = printer.id;
-    //         variants.push(variant);
-    //     }
-    // }
-  
     // Return the product data as props
     return {
       props: {
         item: item.data(),
-        variants: printers
+        variants: variants
       }
     }
 }
