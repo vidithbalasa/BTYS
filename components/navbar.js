09e07a31b6e4124e15a4b0f4a7355ed5dd5608e3
Vidith Balasa <vidithbalasa@Vidiths-MBP.attlocal.net>
2022-12-12 18:00:18 -0800

    Finished navbar styling

--- components/navbar.js
+++ components/navbar.js
@@ -6,26 +6,22 @@ import VinylBackdrop from './vinylBackdrop';
 export default function Navbar({ children }) {
     return (
         <VinylBackdrop>
-            <div className={styles.bar}>
-                <div className={styles.left}>
-                    <div className={styles.logo}>
-                        <Link href='/'>
-                            <a>
-                                <Image src='/Logo.png' alt='Better Than You Society Logo' width={100} height={100} />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className={styles.right}>
-                    <div className={styles.links}>
-                        <Link href='/catalog'><a><h4>Catalog</h4></a></Link>
-                        <Link href='/design'><a><h4>Design</h4></a></Link>
-                        <Link href='/create'><a><h4>Create</h4></a></Link>
-                        <Link href='/portfolio'><a><h4>Portfolio</h4></a></Link>
-                    </div>
-                </div>
+            <div className={styles.logo}>
+                <Link href='/'>
+                    <a>
+                        <Image src='/Logo.png' alt='Better Than You Society Logo' width={200} height={200} />
+                    </a>
+                </Link>
             </div>
-            {children}
+            <div className={styles.links}>
+                <Link href='/catalog'><a><h4>Catalog</h4></a></Link>
+                <Link href='/design'><a><h4>Design</h4></a></Link>
+                <Link href='/create'><a><h4>Create</h4></a></Link>
+                <Link href='/portfolio'><a><h4>Portfolio</h4></a></Link>
+            </div>
+            <div className={styles.content}>
+                {children}
+            </div>
         </VinylBackdrop>
     )
 }
