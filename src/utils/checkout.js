99bf7dd46a9c7eabcc45285fc9d950ed1cec57fa
Vidith Balasa <vidithbalasa@Vidiths-MBP.attlocal.net>
2022-12-14 23:09:53 -0800

    Added metadata to stripe checkout

--- src/utils/checkout.js
+++ src/utils/checkout.js
@@ -1,14 +1,15 @@
 import { getFirestore, collection, addDoc, onSnapshot } from "firebase/firestore";
 import { loadStripe } from "@stripe/stripe-js";
 
-export default async function createSession (firestore, user, line_items) {
+export default async function createSession (firestore, user, line_items, additionalData) {
         const checkout_session_ref = collection(firestore, 'users', user.uid, 'checkout_sessions')
 
         const docRef = await addDoc(checkout_session_ref, {
             mode: 'payment',
             success_url: window.location.origin,
             cancel_url: window.location.origin,
-            line_items
+            line_items,
+            ...additionalData
             // line_items: [{
             //     price_data: {currency: 'usd', product_data: {name: 'testing'}, unit_amount: 10000},
             //     quantity: 1
