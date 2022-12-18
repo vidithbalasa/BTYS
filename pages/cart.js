ed98b4d4e919eb9953e0f8279aebaeac18e03d54
Vidith Balasa <vidithbalasa@Vidiths-MBP.attlocal.net>
2022-12-17 20:07:41 -0800

    tweaks

--- pages/cart.js
+++ pages/cart.js
@@ -11,9 +11,9 @@ import createSession from "../src/utils/checkout";
 
 function Cart() {
     const { user } = useAuth();
-    const [loading, setLoading] = useState(true);
-    const [cartItems, setCartItems] = useState([]);
-    const { line_items, metadata }  = getCartSessionInfo(firestore, user);
+    // const [loading, setLoading] = useState(true);
+    // const [cartItems, setCartItems] = useState([]);
+    // const { line_items, metadata }  = getCartSessionInfo(firestore, user);
     const imageSize = 192;
     const firestore = getFirestore();
     const [checkoutLoad, setCheckoutLoad] = useState(false);
@@ -26,40 +26,40 @@ function Cart() {
         setCheckoutLoad(false);
     }
     
-    // const samplePrompts = [
-    //     'lorem ipsum dolor sit amet. consectetur adipiscing elit. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam',
-    //     'consectetur adipiscing elit',
-    //     'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-    //     'ut enim ad minim veniam',
-    // ]
-    // const [loading, setLoading] = useState(false);
-    // // const cartItems = [{url: sampleImages[0], prompt: samplePrompts[0]}]
-    // const cartItems = []
-    // for (let i = 0; i < 10; i++) {
-    //     cartItems.push({url: sampleImages[i], prompt: samplePrompts[i % 4], quantity: 1, size: '2x2', id: i})
-    // }
+    const samplePrompts = [
+        'lorem ipsum dolor sit amet. consectetur adipiscing elit. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam',
+        'consectetur adipiscing elit',
+        'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        'ut enim ad minim veniam',
+    ]
+    const [loading, setLoading] = useState(false);
+    // const cartItems = [{url: sampleImages[0], prompt: samplePrompts[0]}]
+    const cartItems = []
+    for (let i = 0; i < 10; i++) {
+        cartItems.push({url: sampleImages[i], prompt: samplePrompts[i % 4], quantity: 1, size: '2x2', id: i})
+    }
 
-    useEffect(() => {
-        const getCartItems = async () => {
-            const cartRef = collection(firestore, 'users', user.uid, 'cart');
-            const cartItems = await getDocs(cartRef);
-            const cartItemsArray = [];
-            for (const itemDoc of cartItems.docs) {
-                const data = itemDoc.data();
-                const { quantity, size } = data;
-                const imageRef = data.image;
-                const imageData = await getDoc(imageRef);
-                const { url, prompt } = imageData.data();
-                cartItemsArray.push({url, prompt, quantity, size, id: itemDoc.id});
-            }
-            return cartItemsArray
-        }
-        getCartItems()
-            .then(items => {
-                setCartItems(items);
-                setLoading(false);
-            })
-    }, [user])
+    // useEffect(() => {
+    //     const getCartItems = async () => {
+    //         const cartRef = collection(firestore, 'users', user.uid, 'cart');
+    //         const cartItems = await getDocs(cartRef);
+    //         const cartItemsArray = [];
+    //         for (const itemDoc of cartItems.docs) {
+    //             const data = itemDoc.data();
+    //             const { quantity, size } = data;
+    //             const imageRef = data.image;
+    //             const imageData = await getDoc(imageRef);
+    //             const { url, prompt } = imageData.data();
+    //             cartItemsArray.push({url, prompt, quantity, size, id: itemDoc.id});
+    //         }
+    //         return cartItemsArray
+    //     }
+    //     getCartItems()
+    //         .then(items => {
+    //             setCartItems(items);
+    //             setLoading(false);
+    //         })
+    // }, [user])
 
     if (loading) return <div className={styles.loader}><Loader /></div>
 
