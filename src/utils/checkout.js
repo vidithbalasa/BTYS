e5d1e01363c4c6e97348ee6acb01cd78fb66d13d
Vidith Balasa <vidithbalasa@Vidiths-MBP.attlocal.net>
2022-12-19 14:22:55 -0800

    tweaks

--- src/utils/checkout.js
+++ src/utils/checkout.js
@@ -9,21 +9,23 @@ export default async function createSession (firestore, user, line_items, additi
         success_url: 'https://btys.vercel.app/success?session_id={CHECKOUT_SESSION_ID}',
         cancel_url: window.location.origin,
         shipping_address_collection: {"allowed_countries": ["US"]},
-        shipping_options: [
-            {"shipping_rate_data": {
-                "type": "fixed_amount",
-                "fixed_amount": {"amount": 0, "currency": "usd"},
-                "display_name": "Free shipping",
-                "delivery_estimate": {
-                    "minimum": {"unit": "business_day", "value": 5},
-                    "maximum": {"unit": "business_day", "value": 7},
-                }
-            }}
-        ],
+        shipping_options: [{shipping_rate: "shr_1MGrdnKnHHz9C6fwaktLyliA"}],
         line_items,
         ...additionalData,
     })
 
+    // shipping_options: [
+    //     {"shipping_rate_data": {
+    //         "type": "fixed_amount",
+    //         "fixed_amount": {"amount": 0, "currency": "usd"},
+    //         "display_name": "Free shipping",
+    //         "delivery_estimate": {
+    //             "minimum": {"unit": "business_day", "value": 5},
+    //             "maximum": {"unit": "business_day", "value": 7},
+    //         }
+    //     }}
+    // ],
+    
     onSnapshot(docRef, async (snap) => {
         const { error, sessionId } = snap.data();
         if (error) {
